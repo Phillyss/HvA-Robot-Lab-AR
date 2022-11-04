3f00e6022161654a7c9133a27100fd9b0ae594cb
PVEgmond <philip.vegmond@gmail.com>
2022-11-04 11:25:41 +0100

    finish upload

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const popupAcceptButton = document.querySelector(".tagspopup button");
-const openTagsButton = document.querySelector("fieldset .buttonblue");
+const openTagsButton = document.querySelector("fieldset button.buttonblue");
 const tagsPopup = document.querySelector(".tagspopup");
+const markerCB = document.querySelector("#marker");
+const gpsCB = document.querySelector("#gps");
+const longCon = document.querySelector("#longcon");
+const latCon = document.querySelector("#latcon");
 
-function closeTagsPopup() {
-  tagsPopup.setAttribute("style", "display: none");
+function hideEl(el) {
+  el.classList.add("hidden");
 }
 
-function openTagsPopup() {
-  tagsPopup.setAttribute("style", "display: flex");
+function unhideEl(el) {
+  el.classList.remove("hidden");
 }
 
-popupAcceptButton.addEventListener("click", closeTagsPopup);
-openTagsButton.addEventListener("click", openTagsPopup);
+// switch between AR types
+function checkARType() {
+  if (markerCB.checked) {
+    hideEl(longCon);
+    hideEl(latCon);
+  } else if (gpsCB.checked) {
+    unhideEl(longCon);
+    unhideEl(latCon);
+  }
+}
+
+popupAcceptButton.addEventListener("click", () => hideEl(tagsPopup));
+openTagsButton.addEventListener("click", () => unhideEl(tagsPopup));
+markerCB.addEventListener("change", checkARType);
+gpsCB.addEventListener("change", checkARType);
