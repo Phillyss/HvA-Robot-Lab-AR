5808372eb622ff3ed95e1133d6cb8526e80fad7b
PVEgmond <philip.vegmond@gmail.com>
2022-11-07 17:03:42 +0100

    sidebar 0.9

---
+++ scripts/overview.js
@@ -0,0 +1,29 @@
+const popupAcceptButton = document.querySelector(".popup button");
+const openTagsButton = document.querySelector("aside button.buttonblue");
+const tagsPopup = document.querySelector(".popup");
+
+console.log(popupAcceptButton);
+console.log(openTagsButton);
+console.log(tagsPopup);
+
+function hideEl(el) {
+	el.classList.add("hidden");
+}
+
+function unhideEl(el) {
+	el.classList.remove("hidden");
+}
+
+// switch between AR types
+function checkARType() {
+	if (markerCB.checked) {
+		hideEl(longCon);
+		hideEl(latCon);
+	} else if (gpsCB.checked) {
+		unhideEl(longCon);
+		unhideEl(latCon);
+	}
+}
+
+popupAcceptButton.addEventListener("click", () => hideEl(tagsPopup));
+openTagsButton.addEventListener("click", () => unhideEl(tagsPopup));
