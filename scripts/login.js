8feb9d8e7142dd906bfe983be5b44b50157965fe
PVEgmond <philip.vegmond@gmail.com>
2022-11-08 11:22:04 +0100

    finish log in

---
+++ scripts/login.js
@@ -0,0 +1,7 @@
+const error = document.querySelector("p");
+
+function showError() {
+  error.classList.remove("hidden");
+}
+
+document.querySelector("body").addEventListener("click", (e) => showError());
