c2a90c2ed9ecbf15f11b23d8203ff23a17aff4de
PVEgmond <philip.vegmond@gmail.com>
2022-11-15 16:02:16 +0100

    thumbnail background

--- public/modules/upload.js
+++ public/modules/upload.js
@@ -1,3 +1,5 @@
+const thumbnailInput = document.getElementById("thumbnail");
+const thumbnailLabel = document.querySelector("#thumbnailcon label");
 const popupAcceptButton = document.querySelector(".popup button");
 const openTagsButton = document.querySelector("fieldset button.buttonblue");
 const tagsPopup = document.querySelector(".popup");
@@ -10,43 +12,54 @@ const latCon = document.querySelector("#latcon");
 const cancelButton = document.querySelector(".delpopup .buttonblue");
 
 function hideEl(el) {
-	el.classList.add("hidden");
+  el.classList.add("hidden");
 }
 
 function unhideEl(el) {
-	el.classList.remove("hidden");
+  el.classList.remove("hidden");
 }
 
 // switch between AR types
 function checkARType() {
-	if (markerCB.checked) {
-		hideEl(longCon);
-		hideEl(latCon);
-	} else if (gpsCB.checked) {
-		unhideEl(longCon);
-		unhideEl(latCon);
-	}
+  if (markerCB.checked) {
+    hideEl(longCon);
+    hideEl(latCon);
+  } else if (gpsCB.checked) {
+    unhideEl(longCon);
+    unhideEl(latCon);
+  }
 }
 
 function setTags() {
-	tagsInput.value = "";
-	let newTags = "";
-	allTags.forEach(t => {
-		if (t.checked) {
-			if (newTags.length === 0) {
-				newTags = t.value;
-			} else {
-				newTags = newTags.concat(", ", t.value);
-			}
-		}
-	});
-	tagsInput.value = newTags;
+  tagsInput.value = "";
+  let newTags = "";
+  allTags.forEach(t => {
+    if (t.checked) {
+      if (newTags.length === 0) {
+        newTags = t.value;
+      } else {
+        newTags = newTags.concat(", ", t.value);
+      }
+    }
+  });
+  tagsInput.value = newTags;
 }
 
-popupAcceptButton.addEventListener("click", () => {
-	setTags();
-	hideEl(tagsPopup);
-});
+function setThumbnailBackground() {
+  const file = this.files[0];
+  const reader = new FileReader();
+
+  reader.onloadend = () => {
+    thumbnailLabel.style.backgroundImage = "url(" + reader.result + ")";
+  };
+  if (file) reader.readAsDataURL(file);
+}
+
+thumbnailInput.addEventListener("change", setThumbnailBackground, false);
 openTagsButton.addEventListener("click", () => unhideEl(tagsPopup));
 markerCB.addEventListener("change", checkARType);
 gpsCB.addEventListener("change", checkARType);
+popupAcceptButton.addEventListener("click", () => {
+  setTags();
+  hideEl(tagsPopup);
+});
