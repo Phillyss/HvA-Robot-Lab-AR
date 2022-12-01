a939fdd056fdfb5287b36a388168b0ab1bb112db
PVEgmond <philip.vegmond@gmail.com>
2022-12-01 15:45:13 +0100

    edit, not delete and file update FE

--- public/modules/edit.js
+++ public/modules/edit.js
@@ -1,14 +1,20 @@
-const popupAcceptButton = document.querySelector(".popup button");
+const thumbnailInput = document.getElementById("thumbnail");
+const thumbnailLabel = document.querySelector("#thumbnailcon label");
+const modelInput = document.getElementById("model");
+const popupAcceptButton = document.querySelector(".tagspopup button");
 const openTagsButton = document.querySelector("fieldset button.buttonblue");
-const tagsPopup = document.querySelector(".popup");
+const tagsPopup = document.querySelector(".tagspopup");
+const tagsInput = document.getElementById("tags");
+const allTags = document.querySelectorAll(".tagspopup input");
 const markerCB = document.querySelector("#marker");
 const gpsCB = document.querySelector("#gps");
 const longCon = document.querySelector("#longcon");
 const latCon = document.querySelector("#latcon");
-const delButton = document.querySelector("#buttoncon .buttonpink");
-const delPopup = document.querySelector(".delpopup");
-const deleteCancel = document.querySelector(".delpopup .buttonblue");
-const cancelMain = document.querySelector("#buttoncon .buttongrey");
+const cancelButton = document.querySelector(".delpopup .buttonblue");
+const cancelMain = document.querySelector("#buttoncon button");
+const cancelEditButton = document.querySelector(
+	"#buttoncon button:nth-of-type(2)"
+);
 
 function hideEl(el) {
 	el.classList.add("hidden");
@@ -29,12 +35,61 @@ function checkARType() {
 	}
 }
 
-popupAcceptButton.addEventListener("click", () => hideEl(tagsPopup));
+// hide/unhide coordinates on page load
+checkARType();
+
+// check tags on page load
+const tagsArray = tagsInput.value.split(", ");
+tagsArray.forEach(tag => {
+	const input = document.querySelector(`input[value="${tag}"]`);
+	input.checked = "true";
+});
+
+function setTags() {
+	tagsInput.value = "";
+	let newTags = "";
+	allTags.forEach(t => {
+		if (t.checked) {
+			if (newTags.length === 0) {
+				newTags = t.value;
+			} else {
+				newTags = newTags.concat(", ", t.value);
+			}
+		}
+	});
+	tagsInput.value = newTags;
+}
+
+function setThumbnailBackground() {
+	const file = this.files[0];
+	const reader = new FileReader();
+
+	reader.onloadend = () => {
+		thumbnailLabel.style.backgroundImage = "url(" + reader.result + ")";
+	};
+	if (file) reader.readAsDataURL(file);
+	thumbnailLabel.classList.add("imgselected");
+	thumbnailLabel.innerHTML = "Edit";
+}
+
+function updateModelName() {
+	const file = this.files[0];
+	document.querySelector("fieldset p").innerHTML = file.name;
+}
+
+// get model id
+const urlArray = window.location.href.split("/");
+const modelID = urlArray[urlArray.length - 2];
+
+thumbnailInput.addEventListener("change", setThumbnailBackground, false);
+modelInput.addEventListener("change", updateModelName, false);
 openTagsButton.addEventListener("click", () => unhideEl(tagsPopup));
 markerCB.addEventListener("change", checkARType);
 gpsCB.addEventListener("change", checkARType);
-delButton.addEventListener("click", () => unhideEl(delPopup));
-deleteCancel.addEventListener("click", () => hideEl(delPopup));
-cancelMain.addEventListener("click", () => {
-	window.location.href = "/";
+popupAcceptButton.addEventListener("click", () => {
+	setTags();
+	hideEl(tagsPopup);
+});
+cancelEditButton.addEventListener("click", () => {
+	window.location.href = `/models/${modelID}`;
 });
--- routes/models.js
+++ routes/models.js
@@ -8,6 +8,8 @@ const userModel = require("../schemas/userSchema");
 const qrcode = require("qrcode");
 const { workerData } = require("worker_threads");
 
+// --- FILE UPLOAD ---
+
 // runs before uploading a new model
 let modelsCounter;
 let newModelID;
@@ -59,6 +61,71 @@ const fileStorageEgnine = multer.diskStorage({
 
 const upload = multer({ storage: fileStorageEgnine });
 
+// Edit model: multer
+//let modelsCounter;
+let editModelID;
+router.use("/:id/edit", async (req, res, next) => {
+	try {
+		if (req.method === "POST") {
+			// get model id
+			editModelID = req.params.id;
+			// console.log(req.files);
+
+			// // delete old thumbnail if new one exists
+			// if (req.files) {
+			// 	const newThumbnail = req.files.filter(file => {
+			// 		return file.name.includes("thumbnail");
+			// 	});
+
+			// 	console.log(newThumbnail);
+			// }
+			// console.log("test");
+
+			// const modelDir = fs.promises.readdir(`./appFiles/gltfModel/${editModelID}`, (err, files) => {
+			// 	if (err) {
+			// 		console.log('Unable to scan directory: ' + err);
+			// 	} else {
+
+			// 	}
+
+			// });
+
+			// let modeDir = await fs.promises.mkdir(
+			// 	`./appFiles/gltfModels/${newModelID}`,
+			// 	{ recursive: false },
+			// 	err => {
+			// 		if (err) {
+			// 			if (err.code == "EEXIST") {
+			// 				console.log("Dir already exists");
+			// 				return;
+			// 			}
+			// 		} else {
+			// 			console.log("Dir created");
+			// 		}
+			// 	}
+			// );
+		}
+		next();
+	} catch (err) {
+		console.log(err);
+		next();
+	}
+});
+
+// setup multer: file upload
+const editFSE = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, `./appFiles/gltfModels/${editModelID}`);
+	},
+	filename: (req, file, cb) => {
+		const extArr = file.originalname.split(".");
+		const ext = extArr[extArr.length - 1];
+		cb(null, `${file.fieldname}.${ext}`);
+	},
+});
+
+const edit = multer({ storage: editFSE });
+
 // --- ROUTES ---
 
 // /models: overview page
@@ -82,8 +149,8 @@ router.post(
 		try {
 			// put tags in array
 			const tagsArray = req.body.tags.split(", ");
-			let long = 0;
-			let lat = 0;
+			let long = null;
+			let lat = null;
 
 			// set longitude and latitude if values are provided
 			if (req.body.longitude !== "") long = Number(req.body.longitude);
@@ -117,7 +184,7 @@ router.post(
 				latitude: lat,
 			});
 			const save = await newModel.save();
-			res.redirect("/");
+			res.redirect(`/models/${newModelID}`);
 		} catch (err) {
 			console.log(err);
 		}
@@ -131,14 +198,71 @@ router.get("/:id/edit", async (req, res) => {
 	// if model exists and user is creator > render edit page
 	if (model) {
 		if (req.session.user.id === model.userid) {
-			res.render("pages/edit", { model: model });
+			// get tags
+			let tagsString = "";
+			for (let i = 0; i < model.tags.length; i++) {
+				if (i === 0) {
+					tagsString = model.tags[i];
+				} else {
+					tagsString += `, ${model.tags[i]}`;
+				}
+			}
+
+			res.render("pages/edit", { model: model, tagsString: tagsString });
 		} else {
-			res.redirect("back");
+			res.redirect(`/models/${req.params.id}`);
 		}
 	} else {
 		res.redirect("back");
 	}
 });
+
+// edit new model in db
+router.post(
+	"/:id/edit",
+	edit.fields([
+		{ name: "thumbnail", maxCount: 1 },
+		{ name: "model", maxCount: 1 },
+	]),
+	async (req, res) => {
+		try {
+			// put tags in array
+			const tagsArray = req.body.tags.split(", ");
+			let long = null;
+			let lat = null;
+
+			// set longitude and latitude if values are provided
+			if (req.body.longitude !== "") long = Number(req.body.longitude);
+			if (req.body.latitude !== "") lat = Number(req.body.latitude);
+
+			// set thumbnail file path
+			let thumbnailFilename;
+			const directory = await fs.promises.readdir(
+				`./appFiles/gltfModels/${editModelID}`
+			);
+			thumbnailFilename = directory[1];
+
+			// edit model info in db
+			const editModel = await modelModel.updateOne(
+				{ modelid: editModelID },
+				{
+					$set: {
+						name: req.body.name,
+						thumbnail: thumbnailFilename,
+						description: req.body.description,
+						type: req.body.type,
+						tags: tagsArray,
+						longitude: long,
+						latitude: lat,
+					},
+				}
+			);
+			res.redirect(`/models/${editModelID}`);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+);
 
 // render AR page
 router.get("/:id/ar", async (req, res) => {
