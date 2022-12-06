4328a8bce5dce6a51d286637523e601679f5bf0f
PVEgmond <philip.vegmond@gmail.com>
2022-12-06 16:39:07 +0100

    delete account frontend

--- public/modules/overview.js
+++ public/modules/overview.js
@@ -1,6 +1,23 @@
+// overview/account
 const popupAcceptButton = document.querySelector(".popup button");
 const openTagsButton = document.querySelector("aside button.buttonblue");
 const tagsPopup = document.querySelector(".popup");
+
+// account
+const deleteButton = document.querySelector("h1 + button");
+const deletePopup = document.querySelector(".delpopup");
+const deletePageOne = document.querySelector(".delpopup div:first-of-type");
+const deletePageTwo = document.querySelector(".delpopup div:nth-of-type(2)");
+const deleteInput = document.querySelector(".delpopup input");
+const cancelButtonTwo = document.querySelector(".delpopup div form + button");
+const deleteButtonTwo = document.querySelector(".delpopup div form button");
+
+const cancelButtonOne = document.querySelector(
+	".delpopup div:nth-of-type(1) button:last-of-type"
+);
+const deleteButtonOne = document.querySelector(
+	".delpopup div:nth-of-type(1) button:first-of-type"
+);
 
 function hideEl(el) {
 	el.classList.add("hidden");
@@ -10,16 +27,30 @@ function unhideEl(el) {
 	el.classList.remove("hidden");
 }
 
-// switch between AR types
-function checkARType() {
-	if (markerCB.checked) {
-		hideEl(longCon);
-		hideEl(latCon);
-	} else if (gpsCB.checked) {
-		unhideEl(longCon);
-		unhideEl(latCon);
-	}
-}
-
 popupAcceptButton.addEventListener("click", () => hideEl(tagsPopup));
 openTagsButton.addEventListener("click", () => unhideEl(tagsPopup));
+
+// account page
+if (deleteButton) {
+	deleteButton.addEventListener("click", () => {
+		deleteInput.value = "true";
+		unhideEl(deletePopup);
+	});
+
+	deleteButtonOne.addEventListener("click", () => {
+		hideEl(deletePageOne);
+		unhideEl(deletePageTwo);
+	});
+
+	cancelButtonOne.addEventListener("click", () => {
+		deleteInput.value = "false";
+		hideEl(deletePopup);
+	});
+
+	cancelButtonTwo.addEventListener("click", () => {
+		deleteInput.value = "false";
+		hideEl(deletePopup);
+		hideEl(deletePageTwo);
+		unhideEl(deletePageOne);
+	});
+}
